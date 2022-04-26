bcb5cef8d380befa5ffdf05b12cba4bde8ade36c
Loui <Louiadmin@protonmail.com>
2022-04-26 12:15:43 +1000

    added index and status, but still need to print it in thread

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: Loui :) <loflavel@students.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 21:02:52 by Loui :)           #+#    #+#             */
-/*   Updated: 2022/04/25 22:09:50 by Loui :)          ###   ########.fr       */
+/*   Updated: 2022/04/26 12:14:59 by Loui :)          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 //ATTEMPT #1: PASS THE ID AND STATUS OF PHILOSOPHERS AS A STRUCT
 //A philosopher is represented by a thread. Each philosopher has a unique id & status 
 
-//Philosopher status: 0 - thinking, 1 - eating, 2 - sleeping
+
 //Philosopher status: 0 - thinking, 1 - eating, 2 - sleeping
 //Philosopher status: 0 - thinking, 1 - eating, 2 - sleeping
 //ID is just the index +1
@@ -30,24 +30,55 @@
 
 int mails = 0;
 
-void	ft_write()
+void	ft_putstr(char *s)
 {
-	int i = 0;
-	while (i < 3)
+	int i;
+	
+	i = 0;
+	while (s[i] != '\0')
 	{
-		write(1, &i, 1);
-		i++;
+		write(1, &s[i], 1);
 	}
 }
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int num)
+{
+	if (num < 0)
+	{
+		ft_putchar('-');
+		num = -num;
+	}
+
+	if (num > 9)
+	{
+		ft_putnbr(num / 10);
+		ft_putnbr(num % 10);
+	}
+	else
+		ft_putchar(num + '0');//to convert into 
+}
+int	ft_write(int id)
+{
+	write(1, "#", 1);
+	ft_putnbr(id);
+	write(1, "\n", 1);
+	return (0);
+}
+
 void* routine(void *arg) 
 {
 	usleep(1);
 	t_table *table = (t_table *)arg;
-	printf("Philo %d is awake\n", table->philo->id);
-	// pthread_mutex_lock(table->mutex);
-	// ft_write();
-	// pthread_mutex_unlock(table->mutex);
 	int i;
+	i = 0;
+	pthread_mutex_lock(table->mutex);
+		ft_write(table->philo->id++);//Why does this start at zero & why is it in order?
+    pthread_mutex_unlock(table->mutex);
 	//eat think sleep inside 
 	while(i < 10000000) 
 	{
@@ -59,6 +90,15 @@ void* routine(void *arg)
 	return (0);
 }
 
+void	ft_init_her(t_philo *philo, int i)
+{
+ 		philo[i].thread = malloc(sizeof(pthread_t));
+		philo[i].id = i+1;
+		philo[i].status = 0; //Philosopher status: 0 - thinking, 1 - eating, 2 - sleeping -- create a mutex lock for that
+
+}
+
+
 int main(int argc, char* argv[]) 
 {
 	if (ft_handle_input_errors(argc, argv) != 0)
@@ -68,6 +108,7 @@ int main(int argc, char* argv[])
 	t_philo *philo;
 
 	ft_init_table(argc, argv, &table);
+
 	table.mutex = malloc(sizeof(pthread_mutex_t));//source of orig erre??
     pthread_mutex_init(table.mutex, NULL);//mutex is *ptr, so don't need ampersand
 
@@ -76,17 +117,20 @@ int main(int argc, char* argv[])
 	//philo array ^^
 	int i;
 	i = 0;
-	 
-//array of philos, size no. of philo
+
+// philo = NULL;
    while ( i < table.number_of_philosophers) 
    {
-	   philo[i].thread = malloc(sizeof(pthread_t));
-	
-        if (pthread_create(philo[i].thread, NULL, &routine, &table) != 0) {
-            perror("Failed to create thread");
-            return 1; //th + i same as th[i]
+		// philo[i].thread = malloc(sizeof(pthread_t));
+		// table.philo[i].id = i+1;
+		// table.philo[i].status = 0; //Philosopher status: 0 - thinking, 1 - eating, 2 - sleeping -- create a mutex lock for that
+		ft_init_her(philo, i);
+		if (pthread_create(philo[i].thread, NULL, &routine, &table) != 0) 
+		{
+			perror("Failed to create thread");
+			return 1; //th + i same as th[i]
    		}
-		
+
 		i++;
 	}
 	i = 0;
@@ -96,7 +140,7 @@ int main(int argc, char* argv[])
 		{
             return 2;
         }
-        //printf("Philo %d has finished eating\n", i);
+        printf("Philo %d has finished eating\n", i+1);
 		i++;
     }
     pthread_mutex_destroy(table.mutex);
--- notes_on_threads.c
+++ notes_on_threads.c
@@ -8,67 +8,68 @@ int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 //HERE: Create 10 threads, each taking a UNIQUE prime from the primes array above
 //TRANSLATION TO PHILOSOPHERS: Take each philo and assign her status randomly
 
-void *routine(void *arg)//every single void ptr has the same size
-{
-	sleep(1);
-	int index = *(int *)arg; //cast arg to int ptr, then dereference it
-	printf("%d ", primes[index]);
-	free(arg);
-}
+// void *routine(void *arg)//every single void ptr has the same size
+// {
+// 	sleep(1);
+// 	int index = *(int *)arg; //cast arg to int ptr, then dereference it
+// 	printf("%d ", primes[index]);
+// 	free(arg);
+// }
 
-int main(int argc, char *argv[])
-{
-	pthread_t th[10];
-	int i;
-	int *a;
+// int main(int argc, char *argv[])
+// {
+// 	pthread_t th[10];
+// 	int i;
+// 	int *a;
 
-	i = 0;
-	while (i < 10)
-	{
-		a = malloc(sizeof(int));
-		*a = i; //by giving it an index, you get one of each value in the array
-		if (pthread_create(&th[i], NULL, &routine, a) != 0) //arg3 needs to be ref of i
-			printf("Failed to create thread\n");
-		i++;
-		//can't free here, cos maybe thread hasn't finished execution
-	}
-	i = 0;
-	while (i < 10)
-	{
-		if (pthread_join(th[i], NULL) != 0)
-			printf("Failed to join thread\n");
-		i++;
-	}
-	return (0);
-}
+// 	i = 0;
+// 	while (i < 10)
+// 	{
+// 		a = malloc(sizeof(int));
+// 		*a = i; //by giving it an index, you get one of each value in the array
+// 		if (pthread_create(&th[i], NULL, &routine, a) != 0) //arg3 needs to be ref of i
+// 			printf("Failed to create thread\n");
+// 		i++;
+// 		//can't free here, cos maybe thread hasn't finished execution
+// 	}
+// 	i = 0;
+// 	while (i < 10)
+// 	{
+// 		if (pthread_join(th[i], NULL) != 0)
+// 			printf("Failed to join thread\n");
+// 		i++;
+// 	}
+// 	return (0);
+// }
+
 //RETURNING A VALUE FROM A THREAD --VERY INTERESTING PART ON PTRS TO THREAD VALUES
 
-// void* roll_dice() {
-//     int value = (rand() % 6) + 1; // % is modulo, so it will return a random number between 1 and 6
-//     int* result = malloc(sizeof(int));
-//     *result = value;
-//     // printf("%d\n", value);
-//     printf("Thread result: %p\n", result);
-//     return (void*) result; // need to return a pointer to this LOCAL result.
-// } // Can't return ref to value local to this ft [return (void*) &value;] 
-// //because the stack value is destroyed after the ft returns. This is why need to DYNAMICALLY ALLOCATE
-// // on line 10
+void* roll_dice() {
+    int value = (rand() % 6) + 1; // % is modulo, so it will return a random number between 1 and 6
+    int* result = malloc(sizeof(int));
+    *result = value;
+    // printf("%d\n", value);
+    printf("Thread result: %p\n", result);
+    return (void*) result; // need to return a pointer to this LOCAL result.
+} // Can't return ref to value local to this ft [return (void*) &value;] 
+//because the stack value is destroyed after the ft returns. This is why need to DYNAMICALLY ALLOCATE
+// on line 10
 
-// int main(int argc, char* argv[]) {
-//     int* res;
-//     srand(time(NULL));
-//     pthread_t th;
-//     if (pthread_create(&th, NULL, &roll_dice, NULL) != 0) {
-//         return 1;
-//     }
-//     if (pthread_join(th, (void**) &res) != 0) {
-//         return 2; //2nd argument is a pointer to a pointer, so we need to dereference it
-//     }
-//     printf("Main res: %p\n", res); //ptrs have same addy cos res pts to result of roll_dice(), 
-//     printf("Result: %d\n", *res); //as does result.
-//     free(res);
-//     return 0;
-// }
+int main(int argc, char* argv[]) {
+    int* res;
+    srand(time(NULL));
+    pthread_t th;
+    if (pthread_create(&th, NULL, &roll_dice, NULL) != 0) {
+        return 1;
+    }
+    if (pthread_join(th, (void**) &res) != 0) {
+        return 2; //2nd argument is a pointer to a pointer, so we need to dereference it
+    }
+    printf("Main res: %p\n", res); //ptrs have same addy cos res pts to result of roll_dice(), 
+    printf("Result: %d\n", *res); //as does result.
+    free(res);
+    return 0;
+}
 
 //THREADS CAN BE CREATED FROM OTHER THREADS...
 //THREADS IN A LOOP
