48a365b091961b5c2177e06209db0ff843f6b49d
Loui <Louiadmin@protonmail.com>
2022-04-28 01:27:00 +1000

    finally got structures to work after 3 days :'(

--- main.c
+++ main.c
@@ -1,108 +1,207 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: Loui :) <loflavel@students.42wolfsburg.de> +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/23 21:02:52 by Loui :)           #+#    #+#             */
-/*   Updated: 2022/04/26 22:09:26 by Loui :)          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "philo.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <pthread.h>
-#include <time.h>
+#include <pthread.h> //add -pthread link option otherwise this library won't work
+#include <unistd.h> 
+#include <sys/time.h>
 
-/*MAIN ARGS:
-arg[1] nr_philos
-arg[2] time_die --length of time philo can go w/o eating
-arg[3] time_eat -- eating time, need to hold two forks for this one
-arg[4] time_sleep -- sleep occurs right after eating, first put down forks
-arg[5] (*optional) nr_times_eat -- if all philos have eaten at least X nr of times, simulation stops
-Otherwise sim stops when ONE philo dies
-*/
-//GOAL: create a program that simulates the behaviour of a philosopher eating and thinking hahaha
-//ATTEMPT #1: PASS THE ID AND STATUS OF PHILOSOPHERS AS A STRUCT
-//A philosopher is represented by a thread. Each philosopher has a unique id & status 
-
-
-//Philosopher status: 0 - thinking, 1 - eating, 2 - sleeping
-//Philosopher status: 0 - thinking, 1 - eating, 2 - sleeping
-//ID is just the index +1
-
-//HERE: Create 10 threads, each taking a UNIQUE prime from the primes array above
-//TRANSLATION TO PHILOSOPHERS: Take each philo and assign her status randomly
-
-
-void* routine(void *arg) 
+typedef struct s_vars
 {
-	usleep(1);
-	t_table *table = (t_table *)arg;
+		pthread_mutex_t	mutex;
+}		t_vars;
 
-	//eat think sleep inside
-	//int a = table->philo->id;//this is the 
-	pthread_mutex_lock(table->mutex);
-	//printf("from init philo %d\n", table->philo[i]->id);
-	ft_write(table->philo->id++);//Is it right if it's in order?
-	pthread_mutex_unlock(table->mutex);
-	return (0);
+typedef struct s_philo 
+{
+	int				id;
+	pthread_t 		thread; //make pthread a pointer, then it functions as a dynamic array in this instance
+	//pthread_mutex_t	*mutex;
+}		t_philo;
+//pthread_mutex_t	mutex;//need to lock correct mutex!
+
+
+int		ft_atoi(const char *str);
+size_t	ft_strlen(const char *str);
+void	ft_init_philos(t_philo *philo, int i);
+void	*ft_routine(void *arg);
+int		ft_handle_input_errors(int argc, char *argv[]);
+void	check_digits(char **argv);
+int		ft_isdigit(int c);
+void	ft_putnbr(long n);
+void	ft_write(char *msg)
+{
+	write(1, msg, ft_strlen(msg));
+}
+void	*ft_routine(void *arg)
+{
+	t_philo *philo;
+	t_vars	vars;
+	philo = (t_philo *)arg;
+	///philo->mutex = malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_lock(&vars.mutex);
+	ft_write("Hello from thread id ");
+	ft_putnbr(philo->id);
+	ft_write("\n");
+	//printf("Hello from thread \n");
+	ft_write("Ending thread\n");
+	pthread_mutex_unlock(&vars.mutex);
 }
 
-void	ft_init_her(t_philo *philo, int i)
+int	main(int argc, char *argv[])
 {
- 		philo[i].thread = malloc(sizeof(pthread_t));
-		philo[i].id = i+1;
-		printf("from init philo %d\n", philo[i].id);
-		philo[i].status = 0; //Philosopher status: 0 - thinking, 1 - eating, 2 - sleeping -- create a mutex lock for that
+	int		nr_philos;
+	int		i;
+	t_philo	*philo;
+	t_vars	vars;
 
-}
-
-
-int main(int argc, char* argv[]) 
-{
+	
+	
 	if (ft_handle_input_errors(argc, argv) != 0)
 		return (0);
+	nr_philos = ft_atoi(argv[1]);
+	
+	philo = malloc(sizeof(t_philo) * nr_philos);
 
-	t_table table;
-	//t_philo *philo_struct;
+	
 
-	ft_init_table(argc, argv, &table);
+	pthread_mutex_init(&vars.mutex, NULL);
 
-	table.mutex = malloc(sizeof(pthread_mutex_t));//source of orig erre??
-	pthread_mutex_init(table.mutex, NULL);//mutex is *ptr, so don't need ampersand
-	table.philo = malloc(sizeof(t_philo) * table.number_of_philosophers);
-	//philo_struct = (t_philo *)malloc(sizeof(t_philo) * table.number_of_philosophers);//each philo is a struct
-	//philo array ^^ DBL-UP
-
-// philo = NULL;
-	int i = 0;
-	while ( i < table.number_of_philosophers) 
+	//philo->mutex = malloc(sizeof(pthread_mutex_t) * nr_philos);//it is essential to malloc mutexes. It is a big fucking variable??
+	//init philos as an array of structures
+	i = 0;
+	while (i < nr_philos)
 	{
-		// philo[i].thread = malloc(sizeof(pthread_t));
-		// table.philo[i].id = i+1;
-		// table.philo[i].status = 0; //Philosopher status: 0 - thinking, 1 - eating, 2 - sleeping -- create a mutex lock for that
-		ft_init_her(table.philo, i);
-		//if (pthread_create(philo_struct[i].thread, NULL, &routine, &table) != 0) //mistake is two dif structs
-		if (pthread_create(table.philo[i].thread, NULL, &routine, &table) != 0)
+		
+		ft_init_philos(philo, i);
+		//need a mutex_init for every philo?
+		if (pthread_create(&philo[i].thread, NULL, &ft_routine, &philo[i]) != 0) 
 		{
 			perror("Failed to create thread");
-			return 1; //th + i same as th[i]
-   		}
-
-		i++;
-	}
-	i = 0;
-	while (i < table.number_of_philosophers)
-	{
-		if (pthread_join(*(table.philo[i].thread), NULL) != 0) //deref ptr here
-		{
-			return 2;
+			return 1;
 		}
 		i++;
 	}
-	pthread_mutex_destroy(table.mutex);
-	return 0;
+	
+	i = 0;
+	while (i < nr_philos)
+	{
+		if (pthread_join(philo[i].thread, NULL) != 0) 
+		{
+			write(2, "5\n", 2);
+			return 3;
+		}
+		i++;
+	}
+	pthread_mutex_destroy(&vars.mutex);
+	return (0);
+}
+static void	normi_check_digits(int j)
+{
+	j++;
+	printf("Error: list may only contain digits\n");
+	exit (EXIT_FAILURE);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
+
+void	check_digits(char **argv)
+{
+	char	*c;
+	int		i;
+	int		j;
+
+	i = 1;
+	while (argv[i])
+	{
+		c = argv[i];
+		j = 0;
+		if (ft_strlen(c) == 0)
+			normi_check_digits(j);
+		if (ft_strlen(c) > 1 && c[0] == '-')
+		{
+			printf("Error: list may only contain +ve numbers\n");
+			exit (EXIT_FAILURE);
+		}
+		while (c[j] != '\0')
+		{
+			if (ft_isdigit(c[j]) == 0)
+				normi_check_digits(j);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	ft_handle_input_errors(int argc, char *argv[])
+{
+	check_digits(argv);
+	if (argc != 5 && argc != 6)
+	{
+		printf("Error: too few or too many args entered\n");
+		return (1);
+	}
+	return (0);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_init_philos(t_philo *philo, int i)
+{
+	//ptr[i]->thread = malloc(sizeof(pthread_t));
+	philo[i].id = i + 1;
+	//Philosopher status: 0 - thinking, 1 - eating, 2 - sleeping -- create a mutex lock for that
+}
+
+int	ft_atoi(const char *str)
+{
+	int	minus;
+	int	res;
+	int	i;
+
+	res = 0;
+	minus = 1;
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-')
+	{
+		minus = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		res = (res * 10) + (str[i] - '0');
+		i++;
+	}
+	return (res * minus);
+}
+
+void	ft_putnbr(long n)
+{
+	int		remainder;
+	char	digit;
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+	{
+		digit = n + '0';
+		write(1, &digit, 1);
+	}
 }
--- time.c
+++
@@ -1,8 +0,0 @@
-		struct timeval current_time;
-gettimeofday(&current_time, NULL);
-//arg[0] = time value (struct)
-	//arg[1] = time zone, however obsolete and usually set to NULL
-	long		start_time;
-	 start_time= current_time.tv_usec;
-	  ft_putnbr_fd(start_time);
-	pthread_mutex_init(ptr.mutex, NULL);
